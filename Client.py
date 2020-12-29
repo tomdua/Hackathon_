b2bf9b3955bcde1a8ec1562d721ccb762eb39729
nicole <nicolef@post.bgu.ac.il>
2020-12-29 23:13:53 +0200

    .

--- Client.py
+++ Client.py
@@ -1,23 +1,96 @@
 import socket
+import time
+import signal, os
+import sys, select
+import threading, msvcrt
+import getpass
+import ssl
+from threading import Timer
+from threading import Thread
+import struct
+from random import randrange
+TIMEOUT = 10
+answer = None
+
+
+def check():
+            # print ('You have 10 seconds to type in your stuff...')
+            time.sleep(10)
+            if answer != None:
+                return 
+            # print("Too Slow")
+
+def input():
+    try:
+            # answer = input("Input something: ")
+            # t = Timer(10, timeout)
+            # t.start()
+            print ('You have 10 seconds to type in your stuff...')
+            Thread(target = check).start()
+            ans = getpass.getpass("Input something:"+'\n')
+            # t.join()
+            # print(ans)
+            return ans
+    except:
+            # timeout
+            return "You said nothing!"
 
 def client():
     msgFromClient       = "Hello UDP Server"
     bytesToSend         = str.encode(msgFromClient)
     serverAddressPort   = ("127.0.0.1", 20001)
     bufferSize          = 2048
+    serverPort          = 13117
 
-    
     # Create a UDP socket at client side
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Client started, listening for offer requests...")
+    #GETserver using created UDP socket
+    ixd=randrange(255)
+    UDPClientSocket.bind(("127.0.0.{}".format(ixd), serverPort))
+    while True:
+        message, clientAddress = UDPClientSocket.recvfrom(bufferSize)
+        print(struct.unpack('QQ',message))
+        print("Message from Server: %s"%message)
+    UDPClientSocket.close()
+    
+    # msg = "Message from Server {}".format(msgFromServer[0])
+    # print(msg)
 
-    # Send to server using created UDP socket
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-    msg = "Message from Server {}".format(msgFromServer[0])
-    print(msg)
+    # TCP
+    ClientMultiSocket = socket.socket()
+    host = '127.0.0.1'
+    # port = 2004
+    port = 2001
 
+    print('Waiting for connection response')
+    try:
+        ClientMultiSocket.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+
+    res1 = ClientMultiSocket.recv(1024)
+    time_plus_10 = time.time() + 10
+    if time.time() <= time_plus_10:
+    # while True:
+        print('Start pressing keys on your keyboard as fast as you can!!')
+        # Input = input('Start pressing keys on your keyboard as fast as you can!!')
+        
+
+        # duration is in seconds
+        
+        s = input()
+        # wait for time completion
+
+        ClientMultiSocket.send(str.encode(s))
+        res1 = ClientMultiSocket.recv(1024)
+        res2 = ClientMultiSocket.recv(1024)
+        print(res1.decode('utf-8'))
+        print(res2.decode('utf-8'))
+
+    ClientMultiSocket.close()
 
 if __name__ == "__main__":
     client()
--- Server.py
+++ Server.py
@@ -3,69 +3,95 @@ import threading
 import time
 import struct
 from random import randrange
+import os
+from _thread import *
 
 localIP     = "127.0.0.1"
 localPort   = 20001
 bufferSize  = 2048
+servserPort = 13117
+global score_group1
+global score_group2
+global group1
+global group2
+
+score_group1 = 0
+score_group2 = 0
+group1 = []
+group2 = []
  
+def conn_tcp_theard():
+    # Create a datagram socket
+    TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    # Bind to address and ip
+    TCPServerSocket.bind((localIP, localPort))
+    print("TCP server up and listening")
+    TCPServerSocket.listen(10)
+
 def shutdown_server(UDPServerSocket):
         ''' Shutdown the UDP server '''
         print('Shutting down server...')
         UDPServerSocket.close()
+# def broadcast():
+#     # while True:
+#     #if start game or not
+#     # host_name=socket.gethostname()
+#     # ip=socket.gethostbyname(host_name)
+#     # ip_name =client_address.encode()
+#     # mes= "Server started, listening on IP address " + str(client_address)
+#     # print(mes)
+#     # resp='as'
+#     # time_plus_10 = time.time() + 10
+#     # while time.time() <= time_plus_10:
+    
+#     # UDPServerSocket.sendto(resp.encode('utf-8'), client_address)
+#     # print('\n', resp, '\n')
+#     # UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#     UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#     UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#     UDPServerSocket.bind((localIP, localPort))
+#     UDPServerSocket.settimeout(0.2)
+#     magic_cookie = 0xfeedbeef
+#     message_type= 0x2
+#     message = struct.pack('QQQ', magic_cookie, message_type,localPort)
+#     while True:
+#         UDPServerSocket.sendto(message, ('<broadcast>', 13117))
+#         print('ms_sent')
 
-def broadcast(UDPServerSocket,data, client_address):
-    # while True:
-    #if start game or not
-    # host_name=socket.gethostname()
-    # ip=socket.gethostbyname(host_name)
-    # ip_name =client_address.encode()
-    mes= "Server started, listening on IP address " + str(client_address)
-    print(mes)
-    resp='as'
-    # time_plus_10 = time.time() + 10
-    # while time.time() <= time_plus_10:
-    # UDPServerSocket.sendto(resp.encode('utf-8'), client_address)
-    # print('\n', resp, '\n')
-    UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    magic_cookie = 0xfeedbeef
-    message_type= 0x2
-    message = struct.pack('QQQ', magic_cookie, message_type,localPort)
-    # time_plus_10 = time.time() + 10
-    # while time.time() <= time_plus_10:
-    UDPServerSocket.sendto(message, ("255.255.255.255", 54549))
-    time.sleep(1)
+# def wait_for_client():
+#     ''' Wait for clients and handle their requests '''
+#     UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#     UDPServerSocket.settimeout(10)
+#     # Bind to address and ip
+#     UDPServerSocket.bind((localIP, localPort))
+#     print("UDP server up and listening")
 
-def wait_for_client(UDPServerSocket):
-    ''' Wait for clients and handle their requests '''
-    clients=[]
-    group1=[]
-    group2=[]
-    t_end = time.time() + 10
-    UDPServerSocket.settimeout(t_end - time.time())
-    try:
-        while time.time() < t_end: # keep alive
-            try: # receive request from client
-                data, client_address = UDPServerSocket.recvfrom(1024)
-                c_thread = threading.Thread(target = broadcast,
-                                        args = (UDPServerSocket,data, client_address))
-                c_thread.daemon = True
-                c_thread.start()
-                clients.append(client_address)
-                rondom_team = randrange(2)
-                if rondom_team % 2 == 0:
-                    group1.append(client_address)
-                else:
-                    group2.append(client_address)
-            except OSError as err:
-                print(err)
-    except KeyboardInterrupt:
-        print("Time is up for connecting new players")
-        shutdown_server(UDPServerSocket)
-    return clients,group1,group2
+#     clients=[]
+#     t_end = time.time() + 10
+#     try:
+#         UDPServerSocket.settimeout(t_end - time.time())
+#         while time.time() < t_end: # keep alive
+#             try: # receive request from client
+#                 data, client_address = UDPServerSocket.recvfrom(2024)
+#                 c_thread = threading.Thread(target = broadcast,
+#                                         args = (UDPServerSocket,data, client_address))
+#                 c_thread.daemon = True
+#                 c_thread.start()
+#                 clients.append(client_address)
+#                 rondom_team = randrange(2)
+#                 if rondom_team % 2 == 0:
+#                     group1.append(client_address)
+#                 else:
+#                     group2.append(client_address)
+#             except OSError as err:
+#                 print(err)
+                
+#     except KeyboardInterrupt:
+#         print("Time is up for connecting new players")
+#         shutdown_server(UDPServerSocket)
+#     return clients,group1,group2
 
-
-def start_tcp_server(TCPServerSocket,group1,group2):
+def start_tcp_server():
     print("starting game")
     message = "Welcome to Keyboard Spamming Battle Royale.\n"
     message += "Group 1:\n"
@@ -80,17 +106,76 @@ def start_tcp_server(TCPServerSocket,group1,group2):
     print(message)
     # TCPServerSocket.connect(group1[0])
     # TCPServerSocket.sendall(message.encode())
-
-
- 
-def server():
-    msgFromServer       = "Hello UDP Client"
-    bytesToSend         = str.encode(msgFromServer)
-    # Create a datagram socket
-    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    # Bind to address and ip
+def broadcast():
+    UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     UDPServerSocket.bind((localIP, localPort))
-    print("UDP server up and listening")
+    UDPServerSocket.settimeout(0.5)
+    magic_cookie = 0xfeedbeef
+    message_type= 0x2
+    message = struct.pack('QQ', magic_cookie, message_type)
+    time_plus_10=time.time()+10
+    while time_plus_10>time.time():
+        UDPServerSocket.sendto(message, ('<broadcast>',servserPort))
+        print('Message_sent')
+        time.sleep(1)
+
+def multi_threaded_client(connection):
+        connection.send(str.encode('Server is working:'))
+        # time_plus_10 = time.time() + 10
+        # while time.time() <= time_plus_10:
+        while True:
+            data = connection.recv(2048)
+            msg ='Data From Client:' + data.decode('utf-8')
+            response = 'Server message: Game over!' 
+            if not data:
+                break
+            connection.sendall(str.encode(msg))
+            connection.sendall(str.encode(response))
+        connection.close()
+ 
+
+def server():
+    thread_udp = threading.Thread(target = broadcast,args = ())
+    thread_udp.start()
+    thread_tcp = threading.Thread(target=conn_tcp_theard(), args=())
+    thread_tcp.start()
+
+    ServerSideSocket = socket.socket()
+    host = '127.0.0.1'
+    # port = 2004
+    port = 2001
+    ThreadCount = 0
+    try:
+        ServerSideSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    print('Socket is listening..')
+    ServerSideSocket.listen(5)
+    while True:
+        Client, address = ServerSideSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        start_new_thread(multi_threaded_client, (Client, ))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSideSocket.close()
+
+
+
+    # thread_tcp = threading.Thread(target=conn_tcp_theard(), args=())
+    # thread_tcp.start()
+    # msgFromServer       = "Hello UDP Client"
+    # bytesToSend         = str.encode(msgFromServer)
+    # # Create a datagram socket
+    # UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # # Enable broadcasting mode
+    # UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Bind to address and ip
+    # UDPServerSocket.bind((localIP, localPort))
+    # print("UDP server up and listening")
     # Listen for incoming datagrams
     # while(True):
     #     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -102,21 +187,30 @@ def server():
     #     print(clientIP)
     #     # Sending a reply to client
     #     UDPServerSocket.sendto(bytesToSend, address)
-    clients,group1,group2 = wait_for_client(UDPServerSocket)
-    UDPServerSocket.close()
+   
+   
+    # # Set a timeout so the socket does not block
+    # # indefinitely when trying to receive data.
+    # UDPServerSocket.settimeout(10)
+    # message = b"your very important message"
     # start_time = time.time()
     # while time.time() - start_time < 10:
-    #     UDPServerSocket.close()
+    #     UDPServerSocket.sendto(message, ("127.0.0.1", 20001))
+    #     print("message sent!")
+    #     time.sleep(1)
+    #     clients,group1,group2 = wait_for_client(UDPServerSocket)
+    #     # UDPServerSocket.close()  
+    # UDPServerSocket.close()
 
 
-    msgFromServer       = "Hello TCP Client"
-    bytesToSend         = str.encode(msgFromServer)
-    # Create a datagram socket
-    TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    # Bind to address and ip
-    TCPServerSocket.bind((localIP, localPort))
-    print("TCP server up and listening")
-    start_tcp_server(TCPServerSocket,group1,group2)
+    # msgFromServer       = "Hello TCP Client"
+    # bytesToSend         = str.encode(msgFromServer)
+    # # Create a datagram socket
+    # TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    # # Bind to address and ip
+    # TCPServerSocket.bind((localIP, localPort))
+    # print("TCP server up and listening")
+    #     # # start_tcp_server(TCPServerSocket,group1,group2)
 
 
 if __name__ == "__main__":
