50b1acf9b8f762cdeeecd51751023814cc2a25c5
tomdu <46071208+tomdua@users.noreply.github.com>
2020-12-30 18:21:43 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -9,8 +9,11 @@ from threading import Timer
 from threading import Thread
 import struct
 from random import randrange
-TIMEOUT = 10
+import time
+import tty
+from msvcrt import getch
 answer = None
+import 
 
 def check():
             # print ('You have 10 seconds to type in your stuff...')
@@ -24,8 +27,8 @@ def input():
             # answer = input("Input something: ")
             # t = Timer(10, timeout)
             # t.start()
-            print ('You have 10 seconds to type in your stuff...')
             Thread(target = check).start()
+            print ('You have 10 seconds to type in your stuff...')
             ans = getpass.getpass("Input something:")
             # t.join()
             # print(ans)
@@ -40,55 +43,92 @@ def client():
     serverAddressPort   = ("127.0.0.1", 20001)
     bufferSize          = 2048
     serverPort          = 13117
+    serverTCPPort       = 11117
 
 
-    # # Create a UDP socket at client side
-    # UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # print("Client started, listening for offer requests...")
-    # #GETserver using created UDP socket
-    # ixd=randrange(255)
-    # UDPClientSocket.bind(("127.0.0.{}".format(ixd), serverPort))
-    # while True:
-    #     message, clientAddress = UDPClientSocket.recvfrom(bufferSize)
-    #     print(struct.unpack('QQ',message))
-    #     print("Message from Server: %s"%message)
-    # UDPClientSocket.close()
-    
-    # msg = "Message from Server {}".format(msgFromServer[0])
-    # print(msg)
+    # Create a UDP socket at client side
+    UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Client started, listening for offer requests...")
+    #GETserver using created UDP socket
+    ixd=randrange(255)
+    UDPClientSocket.bind(("127.0.0.{}".format(ixd), serverPort))
+    message, clientAddress = UDPClientSocket.recvfrom(bufferSize)
+    print(struct.unpack('QQ',message))
+    print("Message from Server: %s"%message)
+    address = struct.unpack('QQ',message)
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((clientAddress[0], serverTCPPort))
 
-    # TCP
-    ClientMultiSocket = socket.socket()
-    host = '127.0.0.1'
-    # port = 2004
-    port = 2001
+    tcp_socket.send("Yalla Hiafa.{}".format(ixd).encode("utf-8"))
 
-    print('Waiting for connection response')
-    try:
-        ClientMultiSocket.connect((host, port))
-    except socket.error as e:
-        print(str(e))
+    # tcp_socket.sendall(str.encode('yalha hifa'))
+    # tcp_socket.send(bytes('yalha hifa', encoding='utf8'))
 
-    res = ClientMultiSocket.recv(1024)
-    resres = ClientMultiSocket.recv(1024)
     time_plus_10 = time.time() + 10
-    if time.time() <= time_plus_10:
-    # while True:
-        print(res.decode('utf-8'))
-        print(resres.decode('utf-8'))
-        # print('Start pressing keys on your keyboard as fast as you can!!')
-        # Input = input('Start pressing keys on your keyboard as fast as you can!!')
-        # duration is in seconds
-        s = input()
-        # wait for time completion
+    print1=False
+    # tcp_socket.settimeout(time_plus_10 - time.time())
+    # while True:  # wait for input
+    server_listen_text = tcp_socket.recv(1024)
+    open_client_text = tcp_socket.recv(1024)
+    # pass
+    end = time.time() + 10
+    while time.time() < end:  # wait for input
+        pass
 
-        ClientMultiSocket.send(str.encode(s))
-        res1 = ClientMultiSocket.recv(1024)
-        res2 = ClientMultiSocket.recv(1024)
-        res3 = ClientMultiSocket.recv(1024)
-        print(res1.decode('utf-8'))
-        print(res2.decode('utf-8'))
-        print(res3.decode('utf-8'))
-    ClientMultiSocket.close()
+    print(server_listen_text.decode('utf-8'))
+    print(open_client_text.decode('utf-8'))
+
+    print ('You have 10 seconds to type in your stuff...')
+
+    # settings=termios.tcgetattr(sys.stdin)
+    # try:
+    #     tty.setcbreak(sys.stdin.fileno())
+    #     while True:
+    #         readable, writable, exceptional = select.select([tcp_socket, sys.stdin],[],[],0)
+    #         if tcp_socket in readable:
+    #             try:
+    #                 msg = tcp_socket.recv(bufferSize).decode()
+    #             except ConnectionResetError:
+    #                 break
+    #             if msg:
+    #                 print(msg)
+    #                 break
+    #         if sys.stdin in readable:
+    #             try:
+    #                 char = getch.getch()
+    #             except OverflowError:
+    #                 print('change key')
+    #             try:
+    #                 tcp_socket.send(char.encoded())
+    #             except (BrokenPipeError,ConnectionResetError):
+    #                 print('server close')
+    #                 return
+    # finally:
+    #     termios
+
+
+
+    s = getpass.getpass("Input something:")
+    # print("Input something:")
+    # from msvcrt import getch
+    # time_plus_10 = time.time() + 10
+    # while time.time()<time_plus_10:
+    #     key = getch()
+    # print(key)
+    # s = input()
+    # wait for time completion
+    tcp_socket.send(str.encode(s))
+    data_from_client_txet = tcp_socket.recv(1024)
+    score_text = tcp_socket.recv(1024)
+    finally_text = tcp_socket.recv(1024)
+    print(data_from_client_txet.decode('utf-8'))
+    print(score_text.decode('utf-8'))
+    print(finally_text.decode('utf-8'))
+
+    
+
+
+
+
 if __name__ == "__main__":
     client()
